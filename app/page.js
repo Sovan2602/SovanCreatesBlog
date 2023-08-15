7784989e17bade3c97adc149c69a8a01ad4c160d
SovanCreates <bapali.pattnaik@yahoo.in>
2023-08-15 11:34:42 +0530

    My First Commit of Blog App

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const posts=[
+  {title: 'React Testing', excerpt: 'Learn React Testing'},
+  {title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'}, 
+]
+
+export default function Home() {
+  return (
+   <div className="container mx-auto px-10 mb-8 bg-pink-300">
+    <Head>
+      <title>SovanCreates</title>
+      <link rel="icon" href="/favicon.ico"></link>
+    </Head>
+    <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+      {posts.map((post,index) => (
+        <div>
+          {post.title}
+          {post.excerpt}
+
+        </div>  
+      ))}
+    </div>
+   </div>
+  )
+}
